eb7c9bf4ee2feac06eaf4c0aef1eac4a7a5f2958
ShaheenHuzir <40265125+ShaheenHuzir@users.noreply.github.com>
2020-06-10 14:32:01 +0300

    Updates

---
+++ precache-manifest.0fb188707c482db3bb7db83756a1c70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6f0a99565d75b32981e13d4388849f",
+    "url": "/markdown-app/index.html"
+  },
+  {
+    "revision": "612002fda1bc872e5716",
+    "url": "/markdown-app/static/css/main.3c90a62e.chunk.css"
+  },
+  {
+    "revision": "c760844505688022ab79",
+    "url": "/markdown-app/static/js/2.e290fb6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-app/static/js/2.e290fb6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612002fda1bc872e5716",
+    "url": "/markdown-app/static/js/main.7a467102.chunk.js"
+  },
+  {
+    "revision": "3d3cc7a4161882e694a4",
+    "url": "/markdown-app/static/js/runtime-main.38cbbae0.js"
+  }
+]);
